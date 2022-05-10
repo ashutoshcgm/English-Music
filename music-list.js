16d489d5cd0a7c86843f456c4dcefb7267c518d9
ashutoshcgm <ashutoshexlcgm@gmail.com>
2022-05-10 20:24:00 +0530

    first commit

---
+++ music-list.js
@@ -0,0 +1,67 @@
+
+let allMusic = [
+    {
+        name: "Baby Justin Bieber",
+        artist: "music-1",
+        img: "Music-1",
+        src: "Music-1"
+    },
+    {
+        name: "Bad-habits : Ed Sheeran",
+        artist: "music-2",
+        img: "Music-2",
+        src: "Music-2"
+    },
+    {
+        name: "Love Yourself",
+        artist: "music-3",
+        img: "Music-3",
+        src: "Music-3"
+    },
+    {
+        name: "Let me love You",
+        artist: "music-4",
+        img: "Music-4",
+        src: "Music-4"
+    },
+    {
+        name: "Industry Baby",
+        artist: "music-5",
+        img: "Music-5",
+        src: "Music-5"
+    },
+    {
+        name: "Perfect-Ed Sheeran",
+        artist: "music-6",
+        img: "Music-6",
+        src: "Music-6"
+    }
+    ,
+    {
+        name: "Shape of You",
+        artist: "music-7",
+        img: "Music-7",
+        src: "Music-7"
+    }
+    ,
+    {
+        name: "Stay",
+        artist: "music-8",
+        img: "Music-8",
+        src: "Music-8"
+    }
+    ,
+    {
+        name: "Blank Space",
+        artist: "music-9",
+        img: "Music-9",
+        src: "Music-9"
+    }
+    ,
+    {
+        name: "Bad Blood",
+        artist: "music-10",
+        img: "Music-10",
+        src: "Music-10"
+    }
+];
